c80261e8d6ce0051c9a5b2e2667e32c0bd790a54
Paweł Szumański <pawel.szumanski@outlook.com>
2024-12-17 15:39:02 +0100

    add images to gallery and change images import

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import images from "./images";
+import { images } from "./images";
 import { initImages } from "./create-gallery";
 
+document.querySelector("ul.gallery").appendChild(initImages(images));
