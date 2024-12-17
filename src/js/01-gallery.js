d2bdac22b0614c1001d6f63e6df4db626f50d1aa
Paweł Szumański <pawel.szumanski@outlook.com>
2024-12-17 20:08:31 +0100

    add SimpleLightbox instance

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,3 +4,12 @@ import { images } from "./images";
 import { initImages } from "./create-gallery";
 
 document.querySelector("ul.gallery").appendChild(initImages(images));
+
+const modal = new SimpleLightbox(".gallery-link", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+    
+});
+
+modal.on('show.simplelightbox');
